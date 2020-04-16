a55151918dcfa9e75649f76eb06bf7625e63f174
kawaiian <kawaiian@users.noreply.github.com>
2020-04-16 16:28:17 -0500

    typo in outputs.

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "config" {
         bucket          = aws_s3_bucket.s3_bucket.bucket
         region          = data.aws_region.current.name
         role_arn        = aws_iam_role.iam_role.arn
-        dynamo_db_table = aws_dynamodb_table.dynamodb_table.name
+        dynamodb_table = aws_dynamodb_table.dynamodb_table.name
     }
 }
